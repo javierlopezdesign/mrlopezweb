1425bf1594ad382ed9aea2b3921a662749b6e350
javierlopezdesign <45286457+javierlopezdesign@users.noreply.github.com>
2021-10-18 10:53:24 +0100

    v. 0.3 Favicon added.

--- src/header.js
+++ src/header.js
@@ -12,11 +12,11 @@ function Header() {
         <div className="info__header">
           <p className="grettings__header">
             <Icon className="hand" icon="emojione:waving-hand-medium-light-skin-tone" />
-            Hi there! I'm 
+            Hi there! I'm
           </p>
           <h1>Mr. Lopez</h1>
           <h2>Design & Development</h2>
-          <p>I'm in the process to bring this to life!</p>
+          <p>I'm in the process of bringing this to life!</p>
           {/* <p>Any enquiry just drop me a message down below!</p> */}
           {/* <button className="primaryButton">Primary Button...</button> */}
           {/* <button className="secondaryButton">Secondary Button...</button> */}
